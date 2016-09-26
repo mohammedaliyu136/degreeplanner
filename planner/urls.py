d89bd6049a489969eae76783999f53812db2cc6c
Muhammad <mohammedaliyu136@gmail.com>
2016-09-26 10:35:11 +0100

    added more pages

--- planner/urls.py
+++ planner/urls.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^header/', views.header, name='header'),
 ]
--- planner/views.py
+++ planner/views.py
@@ -7,3 +7,12 @@ def indexx(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
+
+def register(request):
+    return render(request, 'register.html', {})
+
+def header(request):
+    return render(request, 'headerr.html', {})
